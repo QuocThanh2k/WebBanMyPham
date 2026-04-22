80e1a98285d04201211d81ae8b0efa633dff4e47
Quốc Thanh 2k <quocthanh2614@gmail.com>
2026-04-22 09:34:18 +0700

    lan 1

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Cho phép truy cập vào tất cả file trong thư mục gốc (html, css, js, images)
+app.use(express.static(path.join(__WEBMYPHAM__, './')));
+
+// Khi vào link web, nó sẽ tự động mở file index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__WEBMYPHAM__, 'index.html'));
+});
+
+// Render sẽ tự cấp Port qua biến môi trường, nếu không có thì chạy port 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server dang chay tai port ${PORT}`);
+});
