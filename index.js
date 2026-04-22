b84a76aa5afa2f61bf244861f9cd2fd8b557ce6a
Quốc Thanh 2k <quocthanh2614@gmail.com>
2026-04-22 09:49:30 +0700

    kiem tra lai code

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Cho phép truy cập vào tất cả file trong thư mục gốc (html, css, js, images)
-app.use(express.static(path.join(__WEBMYPHAM__, './')));
+app.use(express.static(path.join(__dirname, './')));
 
 // Khi vào link web, nó sẽ tự động mở file index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__WEBMYPHAM__, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Render sẽ tự cấp Port qua biến môi trường, nếu không có thì chạy port 3000
